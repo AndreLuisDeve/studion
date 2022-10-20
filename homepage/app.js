af861da75537b319caeda507042877cb9f9d4f4c
Andre Luis <andrelino85@yahoo.com>
2022-10-20 14:50:55 -0300

    feat(npm): created package.json, gitignore files

---
+++ homepage/app.js
@@ -0,0 +1,8 @@
+const buttonElement = document.getElementById('btn');
+
+buttonElement.addEventListener('click', function (event) {
+
+
+document.getElementById('nome').innerHTML = " Maite ou Guilherme"
+
+})
