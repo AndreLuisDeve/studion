1887024c204d7f18e92ee0e4ac1c4d1ce0f06999
Andre Luis <andrelino85@yahoo.com>
2022-10-20 08:31:29 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const buttonElement = document.getElementById('btn');
+
+buttonElement.addEventListener('click', function (event) {
+
+
+document.getElementById('nome').innerHTML = " Maite ou Guilherme"
+
+
+
+
+
+})
